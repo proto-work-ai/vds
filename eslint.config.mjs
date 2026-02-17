dbff31658fd80ea2bf837eaf3db008568df025b0
Andrey <Andrey@DESKTOP-7K7KR01>
2026-02-17 18:48:34 +0300

    spartan: start

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -15,7 +15,7 @@ export default [
     files: ['**/*.ts', '**/*.tsx', '**/*.js', '**/*.jsx'],
     rules: {
       '@nx/enforce-module-boundaries': [
-        'error',
+        'warn',
         {
           enforceBuildableLibDependency: true,
           allow: ['^.*/eslint(\\.base)?\\.config\\.[cm]?[jt]s$'],
