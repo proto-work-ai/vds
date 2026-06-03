987dc6c3d03a1ec5f89ecf2bfaa5c6aec46fed97
Andrey <Andrey@DESKTOP-7K7KR01>
2026-06-03 22:55:50 +0300

    dm: update

--- apps/deckenmaster/src/app/components/form-store/form-store.directive.ts
+++ apps/deckenmaster/src/app/components/form-store/form-store.directive.ts
@@ -6,20 +6,20 @@ import { FormGroup, FormGroupDirective } from '@angular/forms';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { tap } from 'rxjs';
 
-@Directive({ selector: 'form[formStore]' })
+@Directive({ selector: 'form[formGroupStore]' })
 export class FormStore implements OnInit {
   private readonly injector = inject(Injector);
   private readonly destroyRef = inject(DestroyRef);
   private readonly formGroup = inject(FormGroupDirective);
   private formValue?: ReturnType<typeof injectLocalStorageValue>;
-  formStore = input.required<string>();
+  protected formGroupStore = input.required<string>();
 
   private get form(): FormGroup {
     return this.formGroup.form;
   }
 
   ngOnInit(): void {
-    this.formValue = injectLocalStorageValue(this.formStore(), undefined, this.injector);
+    this.formValue = injectLocalStorageValue(this.formGroupStore(), undefined, this.injector);
 
     if (this.formValue()) {
       const value = this.form.value ?? {};
